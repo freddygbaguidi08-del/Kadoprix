139bdbe7e9a25dfa997ba9d610c631dc8898655a
freddygbaguidi08-del <freddygbaguidi08@gmail.com>
2026-07-21 14:09:49 +0200

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -2,7 +2,7 @@ import type { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [{ userAgent: '*', allow: '/', disallow: ['/api/'] }],
-    sitemap: 'https://promoz.vercel.app/sitemap.xml',
+    rules: [{ userAgent: '*', allow: '/', disallow: ['/api/', '/admin/'] }],
+    sitemap: 'https://kadoprix.vercel.app/sitemap.xml',
   };
 }
